5e5d530f0574d55376c6ad8eb565d6bb8d6e5810
OleksandrPavlyshch <lokdawn80@gmail.com>
2017-04-09 21:09:06 +0300

    update sequence

--- gulp-tasks/default.js
+++ gulp-tasks/default.js
@@ -4,5 +4,7 @@ var gulp = require('gulp')
 	, runSequence = require('run-sequence');
 
 gulp.task('default', function(){
-	runSequence('iconfont', 'favicons', 'fonts', 'scripts', 'sprite', 'images', 'sass','templates', 'server', 'watch');
+	runSequence(
+		['iconfont', 'favicons', 'sprite', 'fonts', 'scripts', 'images']
+		, 'sass','templates', 'server', 'watch');
 });
