b463a5350f958776aa6e34a1f1c4a107eda9079b
OleksandrPavlyshch <lokdawn80@gmail.com>
2018-03-07 09:19:55 +0200

    clean

--- gulp-tasks/tasks/build.js
+++ gulp-tasks/tasks/build.js
@@ -2,11 +2,6 @@ const gulp = require('gulp')
 	, configs = require('../configs')
 	, runSequence = require('run-sequence');
 
-gulp.task('setProdEnv', () => {
-	configs.environment = "prod";
-		}
-	);
-
 gulp.task('build', () => {
 	configs.setEnv('production');
 	configs.logEnv();
